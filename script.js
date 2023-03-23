54901e1b64e88ebf32276ad4987c04951d77c5db
jcc <jccollart@outlook.be>
2023-03-23 13:15:56 +0100

    ok

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+
+
+document.addEventListener ("scroll",() =>{
+   
+
+
+    console.log(window.innerHeight)
+    console.log(window.scrollY)
+    console.log(document.body.offsetHeight)
+
+
+    
+    if (window.scrollY=== 0) {
+        navbar.style.height = "90px"}
+        else {navbar.style.height="45px"};
+        
+        if ((improvise.offsetTop- 90) <= window.pageYOffset  ) {
+            imgImprovise.style.opacity ="1"
+        };
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) 
+        {popup.style.opacity=1
+        popup.style.transform="translateX(0)"}
+    
+    closeBtn.addEventListener("click",()=>{
+       
+
+        popup.remove();
+    })})
+
+
+
 // Créer un événement au scroll
 
 // Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
